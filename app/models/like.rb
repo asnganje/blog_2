c5bca921cd1d1e1de34fcc5ed5bd58fd8646a17d
asnganje <abd.nganje@gmail.com>
2025-03-30 16:27:54 +0300

    fix linter error

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
   after_create :increment_likes_counter
   after_destroy :decrement_likes_counter
-  
+
   private
 
   def increment_likes_counter
