ae539e80a29df721f3c28d248a8f1c04ba83c174
asnganje <abd.nganje@gmail.com>
2025-03-30 16:25:39 +0300

    add logic for auto increment and decrement

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_create :increment_posts_counter
+  after_destroy :decrement_posts_counter
+
+  private
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
+  end
 end
