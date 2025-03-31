e6f0bee4b81a132983890121812fbffc4dbd1043
asnganje <abd.nganje@gmail.com>
2025-03-31 12:55:15 +0300

    fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "users#index"
-  resources :users do 
-    resources :posts, only: [:show, :index, :new, :create]
+  resources :users do
+    resources :posts, only: [ :show, :index, :new, :create ]
   end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
