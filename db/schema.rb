5a46e3d5c8b7690e09f88fd3886113e0e7efc04b
asnganje <abd.nganje@gmail.com>
2025-04-04 16:07:56 +0300

    setup user signup and sign in logic using devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_04_125230) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_130441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_04_125230) do
     t.datetime "confirmation_sent_at"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "unconfirmed_email"
+    t.datetime "remember_created_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
