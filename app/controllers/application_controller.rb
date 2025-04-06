f403e00f6e05d68e5b3fbf1545c1ad09531dfa5b
asnganje <abd.nganje@gmail.com>
2025-04-06 13:37:07 +0300

    Handle denied access in through application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   # end
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue from CanCan::AccessDenied do ||exception
+    redirect_to root_path, alert: exception.message 
+  end
+
   protected
 
   def configure_permitted_parameters
